7d62db1576758d971aca959066edf245c31a2958
Brian McGlothlen <brian@voicebase.com>
2017-11-23 23:41:12 -0800

    Fixed jar file

--- src/main/java/com/voicebase/monitoring/trafficlight/TrafficLightApplication.java
+++ src/main/java/com/voicebase/monitoring/trafficlight/TrafficLightApplication.java
@@ -1,7 +1,6 @@
 package com.voicebase.monitoring.trafficlight;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
@@ -9,7 +8,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
-@EnableAutoConfiguration
+//@EnableAutoConfiguration
 @EnableScheduling
 public class TrafficLightApplication {
   public static void main(String[] args) {
